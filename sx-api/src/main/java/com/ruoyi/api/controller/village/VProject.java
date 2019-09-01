f7ed8998b4d8be1262fdf843e223445ad5333d5b
Abelsjcheng <429433693@qq.com>
2019-09-01 17:42:15 +0800

    添加项目api

--- sx-api/src/main/java/com/ruoyi/api/controller/village/VProject.java
+++ sx-api/src/main/java/com/ruoyi/api/controller/village/VProject.java
@@ -2,9 +2,13 @@ package com.ruoyi.api.controller.village;
 
 import com.ruoyi.api.domain.RongApiRes;
 import com.ruoyi.api.service.RongApiService;
+import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.village.domain.Worklog;
 import com.ruoyi.village.domain.pubObjApi;
 import com.ruoyi.village.service.IProjectService;
 import com.ruoyi.village.domain.Project;
+import com.ruoyi.village.service.IWorklogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +18,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/project")
 @CrossOrigin
 @Api(value = "村务模块 - 项目")
-public class Vproject {
+public class VProject extends BaseController {
     @Autowired
     private IProjectService projectService;
+    @Autowired
+    private IWorklogService workLogService;
 
     @GetMapping("/all")
     @CrossOrigin
@@ -40,5 +46,20 @@ public class Vproject {
     public RongApiRes searchProAll(pubObjApi project )
     {
         return RongApiService.get_list(projectService.selectProjectListForapp(project));
+    }
+
+    @GetMapping("/ListWorkLog")
+    @CrossOrigin
+    @ApiOperation(value = "工作记录列表")
+    public RongApiRes selectWorkLog(pubObjApi workLog){
+        return RongApiService.get_list(workLogService.selectWorklogListByid(workLog));
+    }
+
+    @PostMapping("/insertWorkLog")
+    @CrossOrigin
+    @ApiOperation(value = "新增工作记录")
+    public AjaxResult insertWorkLog(Worklog worklog )
+    {
+        return toAjax(workLogService.insertWorklog(worklog));
     }
 }

8b9d5a18cbe42ae8df11088f325be84c6594addc
Abelsjcheng <429433693@qq.com>
2019-09-18 20:10:38 +0800

    添加链接列表api

--- sx-api/src/main/java/com/ruoyi/api/controller/village/BeaCountry.java
+++ sx-api/src/main/java/com/ruoyi/api/controller/village/BeaCountry.java
@@ -4,16 +4,21 @@ import com.ruoyi.api.domain.RongApiRes;
 import com.ruoyi.api.service.RongApiService;
 import com.ruoyi.broad.service.IOrganizationService;
 import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.common.utils.DateUtil;
 import com.ruoyi.framework.web.base.BaseController;
-import com.ruoyi.village.domain.Villageplan;
-import com.ruoyi.village.domain.Worklog;
-import com.ruoyi.village.domain.pubObjApi;
+import com.ruoyi.village.domain.*;
+import com.ruoyi.village.service.IHouseMemberInfoService;
+import com.ruoyi.village.service.ILinkService;
 import com.ruoyi.village.service.IVillageplanService;
+import com.ruoyi.village.util.bFileUtil1;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,6 +30,8 @@ public class BeaCountry extends BaseController {
     private IVillageplanService villageplanService;
     @Autowired
     private IOrganizationService organizationService;
+    @Autowired
+    private ILinkService linkService;
 
     @GetMapping("/ListBeaCountry")
     @CrossOrigin
@@ -58,9 +65,19 @@ public class BeaCountry extends BaseController {
     @PostMapping("/insertBeaCountry")
     @CrossOrigin
     @ApiOperation(value = "新增美丽乡村")
-    public AjaxResult insertBeaCountry(Villageplan villageplan)
+    public AjaxResult insertBeaCountry(Villageplan villageplan,@RequestParam(value = "files", required = false) MultipartFile file,
+                                       @RequestParam(value = "filename", required = false) String fname,
+                                       @RequestParam(value = "flenth" ,required = false)String flenth, //时长
+                                       @RequestParam(value = "fsize",required = false) String fsize )
     {
         return toAjax(villageplanService.insertVillageplan(villageplan));
     }
 
+    @GetMapping("/Link")
+    @CrossOrigin
+    @ApiOperation(value = "链接列表")
+    public RongApiRes selectLinkList(Link link){
+
+        return RongApiService.get_list(linkService.selectLinkList(link));
+    }
 }

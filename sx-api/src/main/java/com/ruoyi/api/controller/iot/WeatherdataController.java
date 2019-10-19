2dc1b33db8af4bcc722a30b24d85c154cba929ca
H-Eidolon <1601616142@qq.com>
2019-10-19 20:26:30 +0800

    采集天气信息api更改

--- sx-api/src/main/java/com/ruoyi/api/controller/iot/WeatherdataController.java
+++ sx-api/src/main/java/com/ruoyi/api/controller/iot/WeatherdataController.java
@@ -1,24 +1,14 @@
 package com.ruoyi.api.controller.iot;
 
-import java.util.List;
-
 import com.ruoyi.api.domain.RongApiRes;
 import com.ruoyi.api.service.RongApiService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.village.domain.Weatherdata;
 import com.ruoyi.village.service.IWeatherdataService;
 import com.ruoyi.framework.web.base.BaseController;
-import com.ruoyi.common.page.TableDataInfo;
-import com.ruoyi.common.base.AjaxResult;
-import com.ruoyi.common.utils.ExcelUtil;
 /**
  * 采集天气信息 控制层
  *
